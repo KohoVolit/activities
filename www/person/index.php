d2bc076494dc3a6b37502f4e550da6a59206eb63
Michal Skop <michal.skop@kohovolit.eu>
2016-03-29 22:11:37 +0200

    load smarty from vendor

--- www/person/index.php
+++ www/person/index.php
@@ -2,6 +2,8 @@
 
 $settings = json_decode(file_get_contents("../settings.json"));
 
+require '../vendor/autoload.php';
+
 $page = 'person';
 
 $acts = [
@@ -28,7 +30,7 @@ else {
 }
 
 //set up Smarty
-require($settings->smarty_path);
+//require($settings->smarty_path);
 $smarty = new Smarty();
 $smarty->setTemplateDir($settings->app_path . 'smarty/templates');
 $smarty->setCompileDir($settings->app_path . 'smarty/templates_c');
